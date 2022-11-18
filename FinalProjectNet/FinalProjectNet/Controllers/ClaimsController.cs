042c59800cb8b725752e358691f28fbbd133ffdb
INT\Fernando.Giron <fernando.giron@solera.com>
2022-11-18 16:50:01 +0100

    Removed unnecessary comments

--- FinalProjectNet/FinalProjectNet/Controllers/ClaimsController.cs
+++ FinalProjectNet/FinalProjectNet/Controllers/ClaimsController.cs
@@ -43,7 +43,6 @@ namespace FinalProjectNet.Controllers
         }
 
         // PUT: api/Claims/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutClaims(Guid id, Claims claims)
         {
@@ -74,7 +73,6 @@ namespace FinalProjectNet.Controllers
         }
 
         // POST: api/Claims
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Claims>> PostClaims(Claims claims)
         {
--- FinalProjectNet/FinalProjectNet/Controllers/VehiclesController.cs
+++ FinalProjectNet/FinalProjectNet/Controllers/VehiclesController.cs
@@ -43,7 +43,6 @@ namespace FinalProjectNet.Controllers
         }
 
         // PUT: api/Vehicles/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutVehicles(Guid id, Vehicles vehicles)
         {
@@ -74,7 +73,6 @@ namespace FinalProjectNet.Controllers
         }
 
         // POST: api/Vehicles
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Vehicles>> PostVehicles(Vehicles vehicles)
         {
